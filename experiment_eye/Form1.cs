d28c9313c57f66f2576a7c84d0afcdd57fff66f1
Andrés <j-andrezs@hotmail.com>
2017-12-01 20:26:52 -0200

    Add project files.

---
+++ experiment_eye/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tobii.Interaction;
+using System.IO;
+
+namespace experiment_eye
+{
+    public partial class Form1 : Form
+    {
+
+        private static Host _host;
+        private static GazePointDataStream _gazePointDataStream;
+        private static List<double> s1 = new List<double>();
+        public Form1()
+        {
+            
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+            //this.Size = img.Size;
+            this.FormBorderStyle = FormBorderStyle.None;
+            
+            //this.BackgroundImage = img=;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            //this.Size = new System.Drawing.Size(img.Width, img.Height);
+           
+            Cursor.Hide();
+        }
+
+        private static void InitializeHost()
+        {
+            _host = new Host();
+        }
+        private static void DisableConnectionWithTobiiEngine()
+        {
+            _host.DisableConnection();
+        }
+
+        private static void CreateAndVisualizeLightlyFilteredGazePointStream()
+        {
+            _gazePointDataStream = _host.Streams.CreateGazePointDataStream();
+            _gazePointDataStream.GazePoint((x, y, ts) =>
+            {
+                // Console.WriteLine("Timestamp: {0}\tX:{1}, Y:{2}", ts, x, y);
+                s1.Add(ts);
+
+                using (StreamWriter sw = File.AppendText(@"C:\Users\Inf-CG\Documents\WriteLines.txt"))
+                {
+                    sw.WriteLine((ts - s1[0]).ToString("0.#") + ";" + x.ToString("0.##") + ";" + y.ToString("0.##"));
+                }
+            });
+        }
+        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bitmap img = (Bitmap)Image.FromFile(@"C:\Users\Inf-CG\Downloads\123.png", true);
+            WindowState = FormWindowState.Maximized;
+
+
+            PictureBox pbx = new PictureBox();
+            pbx.Image = img;
+            pbx.Size = this.Size;
+            this.Controls.Add(pbx);
+            //pbx.SizeMode = PictureBoxSizeMode.CenterImage;
+            pbx.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            timer.Interval = 30000;
+            InitializeHost();
+            CreateAndVisualizeLightlyFilteredGazePointStream();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            this.Close();
+            DisableConnectionWithTobiiEngine();
+        }
+    }
+}
