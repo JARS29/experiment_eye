b976177b8d18c5befae4748c6291d4fdd41a93c6
jars29 <jandresrsalas@gmail.com>
2018-03-22 21:55:34 -0300

    developed the extracting time for key and eye for each subject

--- processing/probe.py
+++ processing/probe.py
@@ -1,5 +1,4 @@
 import numpy as np
-import time
 import os
 from detectors import fixation_detection, saccade_detection
 from gazeplotter import draw_fixations, draw_heatmap, draw_scanpath, draw_raw
@@ -28,7 +27,6 @@ def extracting_eye(st, x, y, st_rt, rt):
     raw_sent = {}
     data = {}
     lensent = 1
-
     for i in st_rt[1:]:
         data['x'] = []
         data['y'] = []
@@ -45,58 +43,23 @@ def extracting_eye(st, x, y, st_rt, rt):
             data['x'] = []
             data['y'] = []
             data['st'] = []
-            print
-            i, ind
             while ind < len(st):
                 data['x'].append(x[ind])
                 data['y'].append(y[ind])
                 data['st'].append(st[ind])
                 ind += 1
-                if x[ind - 1] - x[ind - 2] > 500 and 1200 < x[ind - 1] < 1400:
-                    st_rt.append(st[ind])
-                    rt.append(1.0)
-                    break
             raw_sent['sent_' + str(lensent)] = data.copy()
             data.clear()
     return raw_sent
 
 
 #
-## extracting RT and time for each sentence (list)
-# def extracting_time(subjects, condition):
-#
-#     time_user={}
-#     times={}
-#     dir = os.path.dirname('__file__')
-#     for i in subjects:
-#             for h in condition:
-#                 filename_eye = os.path.join(dir, 'data_exp', i, h, 'raw_eye.txt')
-#                 filename_rt = os.path.join(dir, 'data_exp', i, h, 'rt.txt')
-#                 st, x, y = import_eyedata(filename_eye)
-#                 st_rt, rt = import_rtdata(filename_rt)
-#                 raw_sent = extracting_eye(st, x, y, st_rt, rt)
-#                 times[h + 'time_eye']= []
-#                 times[h + 'time_key'] = []
-#                 for j in range(1, len(raw_sent) + 1):
-#                     y = (raw_sent['sent_' + str(j)]['st'][-1] - raw_sent['sent_' + str(j)]['st'][0]) / 1000
-#                     time_eye.append(round(y, 2))
-#                 times['time_eye_'+ i + '_'+ condition]= time_eye
-#                 for k in range(0, len(st_rt)):
-#                     if rt[k] == 1 and rt[k - 1] == 0:
-#                         y = ((st_rt[k] - st_rt[k - 1]) / 1000)
-#                         time_key.append([round(y, 2), 0])
-#                     else:
-#                         y = ((st_rt[k] - st_rt[k - 1]) / 1000)
-#                         time_key.append([round(y, 2), 1])
-#             times['time_key_'+ i + '_'+ condition]= time_key
-#     return times
-
-#
 ## Average Eye data for n subjects
-def average_subjects(subjects, condition):  # condition= va or vs
-    n_subj = len(subjects)
-    usr = {}
+def extract_data_subjects(subjects, condition, time_sent=0):  # both lists of subjects and conditions  must be string, sent= 0, time =1
+    usr_sent = {}
+    usr_time={}
     cond= {}
+    times = {}
     dir = os.path.dirname('__file__')
     for i in subjects:
         for h in condition:
@@ -106,22 +69,37 @@ def average_subjects(subjects, condition):  # condition= va or vs
             st_rt, rt = import_rtdata(filename_rt)
             raw = extracting_eye(st, x, y, st_rt, rt)
             cond[h] = raw.copy()
-        usr[i]=cond.copy()
+            if time_sent == 1:
+                times[h] = {}
+                times[h]['time_eye']=[]
+                times[h]['time_key']=[]
+                for k in range(1, len(raw) + 1):
+                    z = np.sum(np.diff(raw['sent_' + str(k)]['st']))/1000
+                    times[h]['time_eye'].append(np.round(z, 2))
+                z = np.diff(st_rt) / 1000
+                times[h]['time_key'].append(np.round(z,2))
+        usr_time[i]=times.copy()
+        times.clear()
+        usr_sent[i]=cond.copy()
         cond.clear()
-    return usr
+    if time_sent:
+        return usr_time
+    else:
+        return usr_sent
 
 #
 ## Ploting
 def visualization_eye(raw_sent, usr, condition, sent, type):  # Type: 0=fixation, 1=scanpath, 2=heatmap
 
     dir = os.path.dirname('__file__')
-    st = np.array(raw_sent['raw_data_' + usr]['sent_' + sent]['st'])
-    x = np.array(raw_sent['raw_data_' + usr]['sent_' + sent]['x'])
-    y = np.array(raw_sent['raw_data_' + usr]['sent_' + sent]['y'])
+    st = np.array(raw_sent[usr][condition]['sent_' + sent]['st'])
+    x = np.array(raw_sent[usr][condition]['sent_' + sent]['x'])
+    y = np.array(raw_sent[usr][condition]['sent_' + sent]['y'])
 
     Sfix, Efix = fixation_detection(x, y, st)
     Ssac, Esac = saccade_detection(x, y, st)
     scrsize = (2560, 1440)
+    print Esac
     dirimage = os.path.join(dir, 'data_exp', usr, condition, 'images', sent + '.png')
     if type == 0:
         draw_fixations(Efix, scrsize, imagefile=dirimage, durationsize=True, durationcolour=False, alpha=0.5,
@@ -138,6 +116,8 @@ def visualization_eye(raw_sent, usr, condition, sent, type):  # Type: 0=fixation
 
 
 
-subjects = ['004' , '007']
-condition= ['va' , 'vs']
-raw_sent = average_subjects(subjects, condition)
+subjects = ['005']
+condition= ['vs', 'va']
+raw_sent = extract_data_subjects(subjects, condition)
+times = extract_data_subjects(subjects, condition, 1)
+
