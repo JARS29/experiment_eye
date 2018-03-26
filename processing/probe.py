532d46ecc7b91d5f5d991383280d0ac196834fd5
jars29 <jandresrsalas@gmail.com>
2018-03-26 19:32:22 -0300

    exporting data

--- processing/probe.py
+++ processing/probe.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import csv
 from detectors import fixation_detection, saccade_detection
 from gazeplotter import draw_fixations, draw_heatmap, draw_scanpath, draw_raw
 
@@ -36,7 +37,7 @@ def extracting_eye(st, x, y, st_rt, rt):
             data['y'].append(y[ind])
             data['st'].append(st[ind])
             ind += 1
-        raw_sent['sent_' + str(lensent)] = data.copy()
+        raw_sent[lensent] = data.copy()
         data.clear()
         lensent += 1
         if i == st_rt[-1]:
@@ -48,7 +49,7 @@ def extracting_eye(st, x, y, st_rt, rt):
                 data['y'].append(y[ind])
                 data['st'].append(st[ind])
                 ind += 1
-            raw_sent['sent_' + str(lensent)] = data.copy()
+            raw_sent[lensent] = data.copy()
             data.clear()
     return raw_sent
 
@@ -63,8 +64,9 @@ def extract_data_subjects(subjects, condition, type=0):  # both lists of subject
     times = {}
     usr_eye_data={}
     eye_data={}
-    dir = os.path.dirname('__file__')
+    #dir = os.path.dirname('__file__')
     #dir= 'C:\Users\JARS\Dropbox'
+    dir= '/home/jars/Dropbox'
     for i in subjects:
         for h in condition:
             filename_eye = os.path.join(dir, 'data_exp', str(i), str(h), 'raw_eye.txt')
@@ -80,18 +82,37 @@ def extract_data_subjects(subjects, condition, type=0):  # both lists of subject
                 times[h]['time_key']=[]
                 for k in range(0, len(rt) - 1):
                     w = (st_rt[k + 1] - st_rt[k]) / 1000
-                    z = ((raw['sent_' + str(k+1)]['st'][-1] - raw['sent_' + str(k+1)]['st'][0]) / 1000)
+                    z = ((raw[k+1]['st'][-1] - raw[k+1]['st'][0]) / 1000)
                     if rt[k] == 0 and rt[k + 1] == 1:
-                        times[h]['time_wait'].append(np.round(w,2))
+                        times[h]['time_wait'].append(str(np.round(w,2)).replace('.',','))
                     else:
-                        times[h]['time_key'].append(np.round(w, 2))
-                        times[h]['time_eye'].append(np.round(z, 2))
-
-
+                        times[h]['time_key'].append(str(np.round(w, 2)).replace('.',','))
+                        times[h]['time_eye'].append(str(np.round(z, 2)).replace('.',','))
             elif type ==2:
                 eye_data[h]={}
+                for j in range(0, len(rt) - 1):
+                    if rt[j] == 0 and rt[j + 1] == 1:
+                        pass
+                    else:
+                        k = j+1
+                        eye_data[h][k]={}
+                        eye_data[h][k]['fixations'] = []
+                        eye_data[h][k]['saccades'] = []
+                        eye_data[h][k]['dur_fix'] = []
+                        eye_data[h][k]['dur_sacc']=[]
+                        st = np.array(raw[k]['st'])
+                        x = np.array(raw[k]['x'])
+                        y = np.array(raw[k]['y'])
+                        Sfix, Efix = fixation_detection(x, y, st)
+                        Ssac, Esac = saccade_detection(x, y, st)
+                        eye_data[h][k]['fixations']=Efix
+                        eye_data[h][k]['saccades'] = Esac
+                        eye_data[h][k]['dur_fix'] = Sfix
+                        eye_data[h][k]['dur_sacc']= Ssac
+            elif type == 3:
+                eye_data[h] = {}
                 for k in raw:
-                    eye_data[h][k]={}
+                    eye_data[h][k] = {}
                     eye_data[h][k]['fixations'] = []
                     eye_data[h][k]['saccades'] = []
                     st = np.array(raw[k]['st'])
@@ -99,7 +120,7 @@ def extract_data_subjects(subjects, condition, type=0):  # both lists of subject
                     y = np.array(raw[k]['y'])
                     Sfix, Efix = fixation_detection(x, y, st)
                     Ssac, Esac = saccade_detection(x, y, st)
-                    eye_data[h][k]['fixations']=Efix
+                    eye_data[h][k]['fixations'] = Efix
                     eye_data[h][k]['saccades'] = Esac
         usr_eye_data[i]=eye_data.copy()
         eye_data.clear()
@@ -111,51 +132,87 @@ def extract_data_subjects(subjects, condition, type=0):  # both lists of subject
         return usr_sent
     elif type==1:
         return usr_time
-    elif type==2:
+    elif type==2 or type==3:
         return usr_eye_data
 
 #
 ## Ploting
 def visualization_eye(raw_sent, usr, condition, sent, type=0):  # Type: 0=fixation, 1=scanpath, 2=heatmap
 
-    dir = os.path.dirname('__file__')
-    Efix= raw_sent[usr][condition]['sent_'+sent]['fixations']
-    Esac= raw_sent[usr][condition]['sent_'+sent]['saccades']
+    #dir = os.path.dirname('__file__')
+    dir= '/home/jars/Dropbox'
+    Efix= raw_sent[usr][condition][sent]['fixations']
+    Esac= raw_sent[usr][condition][sent]['saccades']
     scrsize = (2560, 1440)
     #print (Efix)
     #print (len(Efix))
-    dirimage = os.path.join(dir, 'data_exp', usr, condition, 'images', sent + '.png')
+    dirimage = os.path.join(dir, 'data_exp', usr, condition, 'images', str(sent) + '.png')
     if type == 0:
         draw_fixations(Efix, scrsize, imagefile=dirimage, durationsize=True, durationcolour=False, alpha=0.5,
-                       savefilename=os.path.join(dir, usr + '_' + condition + '_' + sent + "_fixations"))
+                       savefilename=os.path.join(dir, usr + '_' + condition + '_' + str(sent) + "_fixations"))
     elif type == 1:
         draw_scanpath(Efix, Esac, scrsize, imagefile=dirimage, alpha=0.5,
-                      savefilename=os.path.join(dir, usr + '_' + condition + '_' + sent + "_scanpath"))
+                      savefilename=os.path.join(dir, usr + '_' + condition + '_' + str(sent) + "_scanpath"))
     elif type == 2:
         draw_heatmap(Efix, scrsize, imagefile=dirimage, durationweight=True, alpha=0.5,
-                     savefilename=os.path.join(dir, usr + '_' + condition + '_' + sent + "_heatmap"))
+                     savefilename=os.path.join(dir, usr + '_' + condition + '_' + str(sent) + "_heatmap"))
 
 
 # draw_raw(x,y,scrsize, imagefile='original.jpg', savefilename=os.path.join(os.path.dirname(__file__),"raw_data"))
 
+def writing_data(data, subject, condition, type): ###
+    dir= '/home/jars/Dropbox'
+    filename_ord = os.path.join(dir, 'data_exp', subject, condition, 'order.txt')
+    filename_sen =os.path.join(dir, 'data_exp', subject, condition, 'sente.txt')
+    order = np.loadtxt(filename_ord, unpack=True)
+    sent, anw, word = np.genfromtxt(filename_sen, delimiter=';', dtype='str', unpack=True)
+    idx= []
+    for i in order:
+        for j in range(int(i)):
+            idx.append(int(i))
+
+    if type == 0:
+        tim_e=data[subject][condition]['time_eye']
+        tim_k=data[subject][condition]['time_key']
+        tim_w=data[subject][condition]['time_wait']
+        for i in range(len(tim_e)-len(tim_w)):
+            tim_w.append(0)
+        print len(tim_w), len(tim_e), len(tim_k), len(anw), len(word), len(idx)
+        stack= np.stack((anw, word, idx,tim_e,tim_k,tim_w), axis=-1)
+        with open('time_data_' + subject + ' ' + condition + '.csv', 'w') as newFile:
+            newFileWriter = csv.writer(newFile, delimiter=';')
+            for i in stack:
+                newFileWriter.writerow(i)
+    elif type==1:
+        with open('eye_data_' + subject + '_' + condition + '.csv', 'w') as newFile:
+            nfw = csv.writer(newFile, delimiter=';')
+            for j in data[subject][condition]:
+                fix=data[subject][condition][j]['dur_fix']
+                sacc=data[subject][condition][j]['dur_sacc']
+                nfw.writerow('0'+str(j))
+                for i in fix:
+                    nfw.writerow(i)
+                nfw.writerow("###")
+                for i in sacc:
+                    nfw.writerow(i)
 
 
-subjects = ['004']
+
+subjects = ['006','007','009','010','011','012','013','014']
 condition= ['vs', 'va']
 #raw_sent = extract_data_subjects(subjects, condition)
-times = extract_data_subjects(subjects, condition, 1)
-#eye_data = extract_data_subjects(subjects, condition, 2)
+#times = extract_data_subjects(subjects, condition, 1)
+eye_data = extract_data_subjects(subjects, condition, 3)
+# for i in subjects:
+#     if i=='008':
+#         pass
+#     else:
+#         print i
+#         writing_data(times, i, 'vs', 0)
+#         writing_data(times, i, 'va', 0)
 
-#visualization_eye(eye_data, '014', 'vs', '99', 1)
-#visualization_eye(raw_sent, '014', 'va', '99', 1)
-#visualization_eye(raw_sent, '014', 'vs', '99', 2)
-#visualization_eye(raw_sent, '014', 'va', '99', 2)
-#visualization_eye(raw_sent, '010', 'vs', '40', 2)
 
-# visualization_eye(raw_sent, '010', 'vs', '2', 1)
-# visualization_eye(raw_sent, '010', 'vs', '2', 2)
-#
-# visualization_eye(raw_sent, '010', 'vs', '70', 1)
-# visualization_eye(raw_sent, '010', 'vs', '70', 2)
-
+#writing_data(times, '005','vs', 0)
+#writing_data(eye_data,'004','vs',1)
+visualization_eye(eye_data, '006', 'vs', 99, 1)
 
