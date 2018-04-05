d1e0a5b0bc91c1651ca89d1808f85ca38becf04c
jars29 <jandresrsalas@gmail.com>
2018-04-05 19:31:19 -0300

    Fixing csv exporter and comments

--- processing/probe.py
+++ processing/probe.py
@@ -82,13 +82,16 @@ def extract_data_subjects(subjects, condition, type=0):  # both lists of subject
                 times[h]['time_key']=[]
                 for k in range(0, len(rt) - 1):
                     w = (st_rt[k + 1] - st_rt[k]) / 1000
+
                     z = ((raw[k+1]['st'][-1] - raw[k+1]['st'][0]) / 1000)
                     if rt[k] == 0 and rt[k + 1] == 1:
                         times[h]['time_wait'].append(str(np.round(w,2)).replace('.',','))
                     else:
                         times[h]['time_key'].append(str(np.round(w, 2)).replace('.',','))
                         times[h]['time_eye'].append(str(np.round(z, 2)).replace('.',','))
-            elif type ==2:
+                ult = (st[-1] - st_rt[-1])/1000
+                times[h]['time_wait'].append(str(np.round(ult,2)).replace('.',','))
+            elif type ==2: #extracting eye data for each sentence (80) Without wait
                 eye_data[h]={}
                 for j in range(0, len(rt) - 1):
                     if rt[j] == 0 and rt[j + 1] == 1:
@@ -109,7 +112,7 @@ def extract_data_subjects(subjects, condition, type=0):  # both lists of subject
                         eye_data[h][k]['saccades'] = Esac
                         eye_data[h][k]['dur_fix'] = Sfix
                         eye_data[h][k]['dur_sacc']= Ssac
-            elif type == 3:
+            elif type == 3: #extracting eye data for all images (sentences + wait time)
                 eye_data[h] = {}
                 for k in raw:
                     eye_data[h][k] = {}
@@ -162,28 +165,30 @@ def visualization_eye(raw_sent, usr, condition, sent, type=0):  # Type: 0=fixati
 
 def writing_data(data, subject, condition, type): ###
     dir= '/home/jars/Dropbox'
-    filename_ord = os.path.join(dir, 'data_exp', subject, condition, 'order.txt')
-    filename_sen =os.path.join(dir, 'data_exp', subject, condition, 'sente.txt')
-    order = np.loadtxt(filename_ord, unpack=True)
-    sent, anw, word = np.genfromtxt(filename_sen, delimiter=';', dtype='str', unpack=True)
-    idx= []
-    for i in order:
-        for j in range(int(i)):
-            idx.append(int(i))
 
-    if type == 0:
+
+    if type == 0: #extracting results from sente, order, times
+        filename_ord = os.path.join(dir, 'data_exp', subject, condition, 'order.txt')
+        filename_sen = os.path.join(dir, 'data_exp', subject, condition, 'sente.txt')
+        order = np.loadtxt(filename_ord, unpack=True)
+        sent, anw, word = np.genfromtxt(filename_sen, delimiter=';', dtype='str', unpack=True)
+        idx = []
+        for i in order:
+            for j in range(int(i)):
+                idx.append(int(i))
         tim_e=data[subject][condition]['time_eye']
         tim_k=data[subject][condition]['time_key']
         tim_w=data[subject][condition]['time_wait']
         for i in range(len(tim_e)-len(tim_w)):
             tim_w.append(0)
-        print len(tim_w), len(tim_e), len(tim_k), len(anw), len(word), len(idx)
+        #print len(tim_w), len(tim_e), len(tim_k), len(anw), len(word), len(idx)
         stack= np.stack((anw, word, idx,tim_e,tim_k,tim_w), axis=-1)
-        with open('time_data_' + subject + ' ' + condition + '.csv', 'w') as newFile:
+        with open('time_data_' + subject + '_' + condition + '.csv', 'w') as newFile:
             newFileWriter = csv.writer(newFile, delimiter=';')
+            newFileWriter.writerow(['answers','words','order','time_eye','time_key','time_wait'])
             for i in stack:
                 newFileWriter.writerow(i)
-    elif type==1:
+    elif type==1:  #extracting results from eye data
         with open('eye_data_' + subject + '_' + condition + '.csv', 'w') as newFile:
             nfw = csv.writer(newFile, delimiter=';')
             for j in data[subject][condition]:
@@ -198,21 +203,38 @@ def writing_data(data, subject, condition, type): ###
 
 
 
-subjects = ['006','007','009','010','011','012','013','014']
-condition= ['vs', 'va']
+subjects = ['002','003','004','005','006','007','009','010',
+            '011','012','013','014','015','016','017','018',
+            '019','020','021','022','023','024','025','026','027']
+condition= [ 'vs', 'va']
 #raw_sent = extract_data_subjects(subjects, condition)
-#times = extract_data_subjects(subjects, condition, 1)
-eye_data = extract_data_subjects(subjects, condition, 3)
-# for i in subjects:
-#     if i=='008':
-#         pass
-#     else:
-#         print i
-#         writing_data(times, i, 'vs', 0)
-#         writing_data(times, i, 'va', 0)
+times = extract_data_subjects(subjects, condition, 1)
+#tim= extract_data_subjects(subjects,condition,1)
+#eye_data = extract_data_subjects(['003'], condition, 3)
+for i in subjects:
+# #     if i=='008':
+# #         pass
+# #     else:
+     #print i
+     writing_data(times, i, 'vs', 0)
+     writing_data(times, i, 'va', 0)
 
 
-#writing_data(times, '005','vs', 0)
+
 #writing_data(eye_data,'004','vs',1)
-visualization_eye(eye_data, '006', 'vs', 99, 1)
 
+
+#visualization_eye(eye_data, '004', 'va', 17, 2)
+
+
+
+
+#visualization_eye(eye_data, '002', 'vs', 99, 1)
+#visualization_eye(eye_data, '002', 'vs', 98, 1)
+
+#visualization_eye(eye_data, '002', 'vs', 99, 1)
+#visualization_eye(eye_data, '003', 'va', 99, 1)
+#visualization_eye(eye_data, '003', 'vs', 99, 1)
+
+
+#visualization_eye(eye_data, '004', 'vs', 24, 2)
